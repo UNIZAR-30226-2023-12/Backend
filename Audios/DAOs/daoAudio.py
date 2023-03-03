dd20ddfdcdffa4f8787f8a2eec587cb7e8d24800
mariooc04 <mariooc04@gmail.com>
2023-03-03 17:08:47 +0100

    Funciones para obtener los valores de una canción en conjunto o uno en concreto como por ejemplo solo el num veces que se ha reproducido

--- Audios/DAOs/daoAudio.py
+++ Audios/DAOs/daoAudio.py
@@ -4,3 +4,30 @@ import redis
 def guardarCancion(r, id, nombre, artista, calidad, nVeces, val, genero):
     r.hmset(id, {'nombre': nombre, 'artista': artista, 'calidad': calidad, 'nVeces': nVeces, 'val': val, 'generos': genero})
 
+# Funcion para obtener una cancion
+def obtenerCancion(r, id):
+    return r.hgetall(id)
+
+# Funcion para obtener el num de veces que se ha escuchado una cancion
+def obtenerVecesreproducidasCancion(r, id):
+    return r.hget(id, 'nVeces')
+
+# Funcion para obtener la valoracion de una cancion
+def obtenerValCancion(r, id):
+    return r.hget(id, 'val')
+
+# Funcion para obtener el genero de una cancion
+def obtenerGeneroCancion(r, id):
+    return r.hget(id, 'generos')
+
+# Funcion para obtener el nombre de una cancion
+def obtenerNombreCancion(r, id):
+    return r.hget(id, 'nombre')
+
+# Funcion para obtener el artista de una cancion
+def obtenerArtistaCancion(r, id):
+    return r.hget(id, 'artista')
+
+# Funcion para obtener la calidad de una cancion
+def obtenerCalidad(r, id):
+    return r.hget(id, 'calidad')
