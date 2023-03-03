3a8ac7d2f0d920bd996383e9bbf9e6961c4723a4
mariooc04 <mariooc04@gmail.com>
2023-03-03 17:17:34 +0100

    Funciones para actualizar los valores de una cancion

--- Audios/DAOs/daoAudio.py
+++ Audios/DAOs/daoAudio.py
@@ -1,8 +1,40 @@
 import redis
 
+
+#########################################################################################
+#
+#
+# FUNCIONES PARA ALMACENAR/ACTUALIZAR CANCIONES
+#
+#
+#########################################################################################
 # Funcion para guardar una cancion en la base de datos
 def guardarCancion(r, id, nombre, artista, calidad, nVeces, val, genero):
     r.hmset(id, {'nombre': nombre, 'artista': artista, 'calidad': calidad, 'nVeces': nVeces, 'val': val, 'generos': genero})
+
+# Funcion para cambiar el nombre de una cancion
+def cambiarNombreCancion(r, id, nombre):
+    r.hset(id, 'nombre', nombre)
+
+# Funcion para cambiar el artista de una cancion
+def cambiarArtistaCancion(r, id, artista):
+    r.hset(id, 'artista', artista)
+
+# Funcion para cambiar la calidad de una cancion
+def cambiarCalidadCancion(r, id, calidad):
+    r.hset(id, 'calidad', calidad)
+
+# Funcion para cambiar el num de veces que se ha escuchado una cancion
+def cambiarVecesreproducidasCancion(r, id, nVeces):
+    r.hset(id, 'nVeces', nVeces)
+
+# Funcion para cambiar la valoracion de una cancion
+def cambiarValCancion(r, id, val):
+    r.hset(id, 'val', val)
+
+# Funcion para cambiar el genero de una cancion
+def cambiarGeneroCancion(r, id, genero):
+    r.hset(id, 'generos', genero)
 
 # Funcion para obtener una cancion
 def obtenerCancion(r, id):
