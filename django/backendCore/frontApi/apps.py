c474b49d41c5ac79ee872fd3149b455d6a95de8a
816678 <816678@unizar.es>
2023-03-05 00:06:06 +0100

    Añadidos los ficheros fuentes de django, con tres views de prueba

---
+++ django/backendCore/frontApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrontapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'frontApi'
---
+++ django/backendCore/frontApi/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+#import daoAudio
+
+def echo(request):
+    
+    if request.method == 'POST':
+        # Parse the JSON data from the request body
+        json_data = json.loads(request.body)
+        
+        return json_data
+    else:
+        # Return a 405 Method Not Allowed response for other HTTP methods
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+# Create your views here.
+def GetSong(request):
+    id = request.GET.get('idSong')
+    calidadAlta = request.GET.get('calidadAlta')
+
+    # Gets the serialized audio
+    return daoAudio.getCancion(id, calidadAlta)
+
+def SetSong(request):
+
+    if request.method == 'POST':
+        # Parse the JSON data from the request body
+        json_data = json.loads(request.body)
+        
+        # Stores the song in the database
+        daoAudio.guardarCancion(json_data)
+        
+        return True
+    else:
+        # Return a 405 Method Not Allowed response for other HTTP methods
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
