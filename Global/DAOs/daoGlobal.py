c23a368aa935dd4c028f87a5a207cca60ce47b20
mariooc04 <mariooc04@gmail.com>
2023-03-06 17:56:38 +0100

    Añadido control de errores a las funciones de creación/modificado de carpetas

--- Global/DAOs/daoGlobal.py
+++ Global/DAOs/daoGlobal.py
@@ -311,53 +311,133 @@ def obtenerIDListaCancionesPlaylist(r, id):
 
 # Funcion para crear una carpeta
 def crearCarpeta(r, carpetaDic, playlists):
-    id = carpetaDic['id']
-    del carpetaDic['id']
+    # Compruebo que la carpeta tiene todos los atributos necesarios y que playlists es una lista no vacia
+    if 'id' not in carpetaDic or 'nombre' not in carpetaDic or 'usuario' not in carpetaDic or 'publica' not in carpetaDic or 'idListaIDsPlaylist' not in carpetaDic:
+        print('Error: No se han introducido todos los atributos necesarios para crear la carpeta')
+        return -1
+    elif len(playlists) == 0:
+        print('Error: No se han introducido playlists para crear la carpeta')
+        return -1
+    else:
+        crear = True
+        # Compruebo que las playlists existen
+        for playlist in playlists:
+            if not r.exists(playlist):
+                print('Error: La playlist ' + playlist + ' no existe')
+                crear = False
+                return -1
+        if crear:
+            id = carpetaDic['id']
+            del carpetaDic['id']
 
-    # Primero creo la carpeta con sus datos
-    r.hmset(id, carpetaDic)
+            # Primero creo la carpeta con sus datos
+            r.hmset(id, carpetaDic)
 
-    # Creo la lista de playlists
-    r.sadd(carpetaDic['idListaIDsPlaylist'], *playlists)
+            # Creo la lista de playlists
+            r.sadd(carpetaDic['idListaIDsPlaylist'], *playlists)
+    return 0
 
 # Funcion para cambiar el nombre de una carpeta
 def cambiarNombreCarpeta(r, id, nombre):
-    r.hset(id, 'nombre', nombre)
+    # Compruebo que la carpeta existe
+    if not r.exists(id):
+        print('Error: La carpeta ' + id + ' no existe')
+        return -1
+    elif nombre == '':
+        print('Error: El nombre no puede estar vacío')
+        return -1
+    else:
+        r.hset(id, 'nombre', nombre)
+    return 0
 
 # Funcion para cambiar el usuario de una carpeta
 def cambiarUsuarioCarpeta(r, id, usuario):
-    r.hset(id, 'usuario', usuario)
+    # Compruebo que la carpeta existe
+    if not r.exists(id):
+        print('Error: La carpeta ' + id + ' no existe')
+        return -1
+    elif usuario == '':
+        print('Error: El usuario no puede estar vacío')
+        return -1
+    else:
+        r.hset(id, 'usuario', usuario)
+    return 0
 
 # Funcion para cambiar el tipo de carpeta (publica o privada)
 def cambiarPublicaCarpeta(r, id, publica):
-    r.hset(id, 'publica', publica)
+    # Compruebo que la carpeta existe
+    if not r.exists(id):
+        print('Error: La carpeta ' + id + ' no existe')
+        return -1
+    elif publica != 'True' and publica != 'False':
+        print('Error: El tipo de carpeta debe ser True o False')
+        return -1
+    else:
+        r.hset(id, 'publica', publica)
+    return 0
 
 # Funcion para añadir una o más playlists a una carpeta
 def anadirPlaylistCarpeta(r, idCarpeta, idPlaylists):
-    # Primero obtengo la lista de playlists de la carpeta
-    idListaPlay = r.hget(idCarpeta, 'idListaIDsPlaylist')
-
-    # Añado las playlists a la lista
-    r.sadd(idListaPlay, *idPlaylists)
+    # Compruebo que la carpeta existe
+    if not r.exists(idCarpeta):
+        print('Error: La carpeta ' + idCarpeta + ' no existe')
+        return -1
+    else:
+        crear = True
+        # Compruebo que las playlists existen
+        for playlist in idPlaylists:
+            if not r.exists(playlist):
+                print('Error: La playlist ' + playlist + ' no existe')
+                crear = False
+                return -1
+            
+        if crear:
+            # Primero obtengo la lista de playlists de la carpeta
+            idListaPlay = r.hget(idCarpeta, 'idListaIDsPlaylist')
+        
+            # Añado las playlists a la lista
+            r.sadd(idListaPlay, *idPlaylists)
+    return 0
 
 # Funcion para eliminar una o más playlists de una carpeta
 def eliminarPlaylistCarpeta(r, idCarpeta, idPlaylists):
-    # Primero obtengo la lista de playlists de la carpeta
-    idListaPlay = r.hget(idCarpeta, 'idListaIDsPlaylist')
-
-    # Elimino las playlists de la lista
-    r.srem(idListaPlay, *idPlaylists)
+    # Compruebo que la carpeta existe
+    if not r.exists(idCarpeta):
+        print('Error: La carpeta ' + idCarpeta + ' no existe')
+        return -1
+    else:
+        crear = True
+        # Compruebo que las playlists existen
+        for playlist in idPlaylists:
+            if not r.exists(playlist):
+                print('Error: La playlist ' + playlist + ' no existe')
+                crear = False
+                return -1
+            
+        if crear:
+            # Primero obtengo la lista de playlists de la carpeta
+            idListaPlay = r.hget(idCarpeta, 'idListaIDsPlaylist')
+        
+            # Elimino las playlists de la lista
+            r.srem(idListaPlay, *idPlaylists)
+    return 0
 
 # Funcion para eliminar una carpeta
 def eliminarCarpeta(r, id):
-    # Primero obtengo la lista de playlists de la carpeta
-    idListaPlay = r.hget(id, 'idListaIDsPlaylist')
-
-    # Elimino la carpeta
-    r.delete(id)
-
-    # Elimino la lista de playlists de la carpeta
-    r.delete(idListaPlay)
+    # Compruebo que la carpeta existe
+    if not r.exists(id):
+        print('Error: La carpeta ' + id + ' no existe')
+        return -1
+    else:    
+        # Primero obtengo la lista de playlists de la carpeta
+        idListaPlay = r.hget(id, 'idListaIDsPlaylist')
+    
+        # Elimino la carpeta
+        r.delete(id)
+    
+        # Elimino la lista de playlists de la carpeta
+        r.delete(idListaPlay)
+    return 0
 
 #########################################################################################
 #
