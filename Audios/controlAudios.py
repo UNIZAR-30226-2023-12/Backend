574cfcc2eb303ef18ef8eb0360f443a060a312c0
mariooc04 <mariooc04@gmail.com>
2023-03-25 15:36:04 +0100

    Implementadas en el módulo controlAudios funciones para podcasts En concreto, las que se encargan de obtener/modificar/eliminar/añadir podcasts haciendo uso de las funciones ya implementadas en los daos

--- Audios/controlAudios.py
+++ Audios/controlAudios.py
@@ -19,39 +19,29 @@ from DAOS import daoAudio
 
 # Función para almacenar una canción
 def almacenarCancion(r, cancionDic):
-    # Primero compruebo que la canción no existe
-    if r.exists(cancionDic['id']):
-        print('Error: La canción ' + cancionDic['id'] + ' ya existe')
-        return -1
-    else:
-        # Almaceno la canción
-        return daoAudio.guardarCancion(r, cancionDic)
+    # Almaceno la canción
+    return daoAudio.guardarCancion(r, cancionDic)
     
 # Función para cambiar los valores de una canción
 # Si alguno de los valores no se quiere cambiar, se debe pasar como parámetro None (salvo el id)
 def cambiarAtributosCancion(r, id, nombre, artista, calidad, nVeces, val, generos, ficheroAltaCalidad, ficheroBajaCalidad):
-    # Compruebo que la canción existe
-    if r.exists(id):
-        if nombre != None:
-            daoAudio.cambiarNombreCancion(r, id, nombre)
-        if artista != None:
-            daoAudio.cambiarArtistaCancion(r, id, artista)
-        if calidad != None:
-            daoAudio.cambiarCalidadCancion(r, id, calidad)
-        if nVeces != None:
-            daoAudio.cambiarVecesreproducidasCancion(r, id, nVeces)
-        if val != None:
-            daoAudio.cambiarValCancion(r, id, val)
-        if generos != None:
-            daoAudio.cambiarGeneroCancion(r, id, generos)
-        if ficheroAltaCalidad != None:
-            daoAudio.cambiarFicheroAltaCalidad(r, id, ficheroAltaCalidad)
-        if ficheroBajaCalidad != None:
-            daoAudio.cambiarFicheroBajaCalidad(r, id, ficheroBajaCalidad)
-        return 0
-    else:
-        print('Error: La canción ' + id + ' no existe')
-        return -1
+    if nombre != None:
+        daoAudio.cambiarNombreCancion(r, id, nombre)
+    if artista != None:
+        daoAudio.cambiarArtistaCancion(r, id, artista)
+    if calidad != None:
+        daoAudio.cambiarCalidadCancion(r, id, calidad)
+    if nVeces != None:
+        daoAudio.cambiarVecesreproducidasCancion(r, id, nVeces)
+    if val != None:
+        daoAudio.cambiarValCancion(r, id, val)
+    if generos != None:
+        daoAudio.cambiarGeneroCancion(r, id, generos)
+    if ficheroAltaCalidad != None:
+        daoAudio.cambiarFicheroAltaCalidad(r, id, ficheroAltaCalidad)
+    if ficheroBajaCalidad != None:
+        daoAudio.cambiarFicheroBajaCalidad(r, id, ficheroBajaCalidad)
+    return 0
     
 # Función para eliminar una canción
 def borrarCancion(r, id):
@@ -100,3 +90,85 @@ def obtenerAltaCalidadCancion(r, id):
 # Función para obtener el fichero de baja calidad de una canción
 def obtenerBajaCalidadCancion(r, id):
     return daoAudio.obtenerFicheroBajaCalidad(r, id)
+
+##############################################################################################################
+#
+#
+#   Funciones que controlan el modificado/añadido/eliminado de podcasts
+#
+#
+##############################################################################################################
+
+# Función para almacenar un podcast
+def almacenarPodcast(r, podcastDic):
+    # Almaceno el podcast
+    return daoAudio.guardarPodcast(r, podcastDic)
+
+# Función para cambiar los valores de un podcast
+# Si alguno de los valores no se quiere cambiar, se debe pasar como parámetro None (salvo el id)
+def cambiarAtributosPodcast(r, id, nombre, artista, calidad, nVeces, val, descripcion, ficheroAltaCalidad, ficheroBajaCalidad, episodios):
+    if nombre != None:
+        daoAudio.cambiarNombrePodcast(r, id, nombre)
+    if artista != None:
+        daoAudio.cambiarArtistaPodcast(r, id, artista)
+    if calidad != None:
+        daoAudio.cambiarCalidadPodcast(r, id, calidad)
+    if nVeces != None:
+        daoAudio.cambiarVecesreproducidasPodcast(r, id, nVeces)
+    if val != None:
+        daoAudio.cambiarValPodcast(r, id, val)
+    if descripcion != None:
+        daoAudio.cambiarDescPodcast(r, id, descripcion)
+    if ficheroAltaCalidad != None:
+        daoAudio.cambiarFicheroAltaCalidadPodcast(r, id, ficheroAltaCalidad)
+    if ficheroBajaCalidad != None:
+        daoAudio.cambiarFicheroBajaCalidadPodcast(r, id, ficheroBajaCalidad)
+    return 0
+
+# Función para eliminar un podcast
+def borrarPodcast(r, id):
+    return daoAudio.borrarPodcast(r, id)
+
+##############################################################################################################
+#
+#
+#   Funciones que controlan la obtención de datos de podcasts
+#
+#
+##############################################################################################################
+
+# Función para obtener todos los datos de un podcast
+def obtenerTodosPodcast(r, id):
+    return daoAudio.obtenerPodcast(r, id)
+
+# Función para obtener el número de veces que se ha reproducido un podcast
+def obtenerVecesReproducidasPodcast(r, id):
+    return daoAudio.obtenerVecesreproducidasPodcast(r, id)
+
+# Función para obtener la valoración de un podcast
+def obtenerValoracionPodcast(r, id):
+    return daoAudio.obtenerValPodcast(r, id)
+
+# Función para obtener la descripción de un podcast
+def obtenerDescPodcast(r, id):
+    return daoAudio.obtenerDescPodcast(r, id)
+
+# Función para obtener el nombre de un podcast
+def obtenerNomPodcast(r, id):
+    return daoAudio.obtenerNombrePodcast(r, id)
+
+# Función para obtener el artista de un podcast
+def obtenerArtPodcast(r, id):
+    return daoAudio.obtenerArtistaPodcast(r, id)
+
+# Función para obtener la calidad de un podcast
+def obtenerCalPodcast(r, id):
+    return daoAudio.obtenerCalidadPodcast(r, id)
+
+# Función para obtener el fichero de alta calidad de un podcast
+def obtenerAltaCalidadPodcast(r, id):
+    return daoAudio.obtenerFicheroAltaCalidadPodcast(r, id)
+
+# Función para obtener el fichero de baja calidad de un podcast
+def obtenerBajaCalidadPodcast(r, id):
+    return daoAudio.obtenerFicheroBajaCalidadPodcast(r, id)
