fc0048ad2ce06ea42b61a8c610a7d5b871855b27
hsunekichi <hsunekichi@gmail.com>
2023-04-24 11:50:13 +0200

    Arreglado GetUsers

--- integracion/prueba.py
+++ integracion/prueba.py
@@ -4,11 +4,14 @@ import base64
 from django.middleware import csrf
 from django.http import HttpRequest
 
+ip = '127.0.0.1'
+port = '8000'
+
 # URL del endpoint SetSong
-url_set_song = 'http://0.0.0.0:8081/SetSong/'
-url_set_usr = 'http://0.0.0.0:8081/SetUser/'
-url_get_usr = 'http://127.0.0.1:8081/GetUser/'
-url_get = 'http://0.0.0.0:8081/GetSong/'
+url_set_song = 'http://'+ip+':'+port+'/SetSong/'
+url_set_usr = 'http://'+ip+':'+port+'/SetUser/'
+url_get_usr = 'http://'+ip+':'+port+'/GetUser/'
+url_get = 'http://'+ip+':'+port+'/GetSong/'
 
 # Crear un objeto HttpRequest vacío
 # request = HttpRequest()
@@ -58,14 +61,14 @@ params_get_song = {
 }
 
 params_get_usr = {
-    'idUsr': 'usuario:2',
+    'idUsr': 'usuario:1',
     'contrasenya': '1234'
 }
 
 
 
 # Realizar la petición HTTP POST
-#response = requests.post(url_set_song, json=new_song_data)
+response = requests.post(url_set_song, json=new_song_data)
 response = requests.get(url_get, params=params_get_song)
 usr = requests.get(url_get_usr, params=params_get_usr)
 print(usr.json())
